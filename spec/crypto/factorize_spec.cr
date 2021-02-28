6700b65c9467f228b2ed6d539e42cc7a60705f75
Chris Watson <cawatson1993@gmail.com>
2021-02-27 23:07:16 -0700

    Fix issue with 2fa module

--- spec/crypto/factorize_spec.cr
+++ spec/crypto/factorize_spec.cr
@@ -3,11 +3,11 @@ require "../spec_helper"
 Spectator.describe Proton::Crypto::Factorize do
   it "performs prime factorization 1" do
     pq = described_class.factorize(1470626929934143021)
-    expect(pq).to eq({ 1206429347, 1218991343 })
+    expect(pq).to eq({1206429347, 1218991343})
   end
 
   it "performs prime factorization 2" do
     pq = described_class.factorize(2363612107535801713)
-    expect(pq).to eq({ 1518968219, 1556064227 })
+    expect(pq).to eq({1518968219, 1556064227})
   end
 end
--- src/proton/crypto/2fa.cr
+++
@@ -1,5 +0,0 @@
-module Proton::Crypto
-  module 2FA
-    # TODO
-  end
-end
